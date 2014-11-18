0179ebabcca07ec4b5224e6a328e80c12473633b
Marcus Pinnecke <marcus.pinnecke@gmail.com>
2014-11-18 16:43:24 +0100

    OS X and Linux compatible project

--- Medusa-MacOS/Game1.cs
+++ Medusa-MacOS/Game1.cs
@@ -1,34 +1,10 @@
-﻿#region File Description
-//-----------------------------------------------------------------------------
-// MedusaMacOSGame.cs
-//
-// Microsoft XNA Community Game Platform
-// Copyright (C) Microsoft Corporation. All rights reserved.
-//-----------------------------------------------------------------------------
-using Medusa;
-
-
-#endregion
-
-#region Using Statements
-using System;
-
+﻿using Medusa;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Graphics;
-using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Input.Touch;
-using Microsoft.Xna.Framework.Storage;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Media;
 
-#endregion
+using System;
 
 namespace MedusaMacOS
 {
-	/// <summary>
-	/// Default Project Template
-	/// </summary>
 	public class Game1 : Renderer
 	{
 
@@ -36,19 +12,12 @@ namespace MedusaMacOS
 		{
 		}
 
-		/// <summary>
-		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
-		/// we'll use the viewport to initialize some values.
-		/// </summary>
 		protected override void Initialize ()
 		{
 			base.Initialize ();
 		}
 
 
-		/// <summary>
-		/// Load your graphics content.
-		/// </summary>
 		protected override void LoadContent ()
 		{
 			base.LoadContent ();
@@ -56,29 +25,14 @@ namespace MedusaMacOS
 
 	
 
-		#region Update and Draw
-
-		/// <summary>
-		/// Allows the game to run logic such as updating the world,
-		/// checking for collisions, gathering input, and playing audio.
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Update (GameTime gameTime)
 		{
-			// TODO: Add your update logic here			
-            		
 			base.Update (gameTime);
 		}
 
-		/// <summary>
-		/// This is called when the game should draw itself. 
-		/// </summary>
-		/// <param name="gameTime">Provides a snapshot of timing values.</param>
 		protected override void Draw (GameTime gameTime)
 		{
 			base.Draw (gameTime);
 		}
-
-		#endregion
 	}
 }
